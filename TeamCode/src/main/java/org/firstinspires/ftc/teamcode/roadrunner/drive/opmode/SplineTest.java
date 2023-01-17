0bbc53b2edeb062d2d8b760ce0e14cba061e2a47
Parthiv Shah <42523433+ParthivS20@users.noreply.github.com>
2023-01-17 13:56:21 -0600

    Update SplineTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/SplineTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/SplineTest.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.roadrunner.drive.MecanumDriveOdometry;
 /*
  * This is an example of a more complex path to really test the tuning.
  */
-@Autonomous(group = "Roadrunner Config")
+@Autonomous(group = "2")
 public class SplineTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
